f903200365547935b024f47502a45aa46e530875
Sunil Saklani <sunil.saklani@webelementinc.com>
2026-09-01 14:16:37 +0530

    010926

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,12 +2,9 @@ export const environment = {
   production: false,
   apiBaseUrl: 'http://localhost:9091/taskapp/',
   baseurluploaded: 'https://app.webelement.cc/taskapp/uploads/',
-  
+
   // apiBaseUrl: 'https://app.webelement.cc/taskapp_api/',
   // baseurluploaded: 'https://app.webelement.cc/taskapp/uploads/',
-
-  apiBaseUrl: 'https://app.webelement.cc/taskapp_api/',
-  baseurluploaded: 'https://app.webelement.cc/taskapp/uploads/',
   recordsPerPage: 10,
   size: 10,
 };
