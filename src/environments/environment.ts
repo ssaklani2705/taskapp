2c69678324ae96a66b1d3a430de193e43d0f1bba
Kaushal Panchal <kaushal.panchal@webelementinc.com>
2026-09-01 10:07:28 +0530

    010926

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: false,
   apiBaseUrl: 'http://localhost:9091/taskapp/',
   baseurluploaded: 'https://app.webelement.cc/taskapp/uploads/',
-
+  
   // apiBaseUrl: 'https://app.webelement.cc/taskapp_api/',
   // baseurluploaded: 'https://app.webelement.cc/taskapp/uploads/',
   recordsPerPage: 10,
