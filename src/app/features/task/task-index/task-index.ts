9604776072d205c63739156c470b61c21ab61187
Kaushal Panchal <kaushal.panchal@webelementinc.com>
2026-09-23 10:13:51 +0530

    230926

--- src/app/features/task/task-index/task-index.ts
+++ src/app/features/task/task-index/task-index.ts
@@ -1936,7 +1936,7 @@ export class TaskIndex {
       note: this.taskNote.trim(),
       userId: this.userId,
       sendMail: this.sendMail,
-      isAdmin:this.isAdmin,
+      isAdmin: this.isAdmin,
     };
 
     this.dataprovider.addTaskNote(request).subscribe({
@@ -2500,14 +2500,7 @@ export class TaskIndex {
   }
 
 
-  @HostListener('document:click', ['$event'])
-  onDocumentClick(event: MouseEvent): void {
-    const target = event.target as HTMLElement;
 
-    if (!target.closest('.status-dropdown')) {
-      this.showStatusDropdown = false;
-    }
-  }
 
 
   //Assign To Model
@@ -2685,9 +2678,9 @@ export class TaskIndex {
   canDisableChangeManager(task: any): boolean {
 
     // Admin can perform any action
-      if (this.isAdmin === 'Y') {
-        return false;
-      }
+    if (this.isAdmin === 'Y') {
+      return false;
+    }
 
     const isManager = Number(task.managerId) === Number(this.userId);
 
@@ -2821,14 +2814,7 @@ export class TaskIndex {
     this.onSearch();
   }
 
-  @HostListener('document:click', ['$event'])
-  onDocumentClick2(event: Event): void {
-    const target = event.target as HTMLElement;
-
-    if (!target.closest('.client-dropdown-container')) {
-      this.showClientDropdown = false;
-    }
-  }
+  
 
   clearClientSearch(): void {
 
@@ -2842,6 +2828,29 @@ export class TaskIndex {
     this.onSearch(); // Reload all records
   }
   sendMail: boolean = false;
+  showTaskCategoryDropdown = false;
+
+
+   @HostListener('document:click', ['$event'])
+  onDocumentClick(event: Event): void {
+
+    const target = event.target as HTMLElement;
+
+    // Status dropdown
+    if (!target.closest('.status-dropdown')) {
+      this.showStatusDropdown = false;
+    }
+
+    // Client dropdown
+    if (!target.closest('.client-dropdown-container')) {
+      this.showClientDropdown = false;
+    }
+
+    // Task Category dropdown
+    if (!target.closest('.task-category-dropdown')) {
+      this.showTaskCategoryDropdown = false;
+    }
+  }
 }
 
 
